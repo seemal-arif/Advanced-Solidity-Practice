295fd0b4cd8262b4dd08a18434e607765f7a7014
Seemal Arif <99790333+seemal-arif@users.noreply.github.com>
2023-02-13 13:04:03 +0500

    Update Mapping.sol

--- Mapping.sol
+++ Mapping.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract Mapping{
-
+ // if we right public with this map it will act as getter as well if not then you have to make a getter function as well
     mapping(address=>mapping(address=>uint)) public arr;
 
     function set(address a,address b,uint i)public
@@ -14,6 +14,9 @@ contract Mapping{
         return arr[a][b];
     }
 
-
+   function remove(address a,address b)public
+   {
+   delete arr[a][b];
+   }
 
 }
